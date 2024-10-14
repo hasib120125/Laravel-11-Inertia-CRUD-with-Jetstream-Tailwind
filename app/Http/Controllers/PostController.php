93eebfd09ede61269d3b7d5e210537f01dce8aab
Md Hasibul Islam <hasib.pust.cse@gmail.com>
2024-10-14 17:26:32 +0600

    update

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+
+    {
+
+        $data = Post::all();
+
+        return Inertia::render('posts', ['data' => $data]);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Validator::make($request->all(), [
+            'title' => ['required'],
+            'body' => ['required'],
+        ])->validate();
+
+        Post::create($request->all());
+
+        return redirect()->back()->with('message', 'Post Created Successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        Validator::make($request->all(), [
+            'title' => ['required'],
+            'body' => ['required'],
+        ])->validate();
+
+        if ($request->has('id')) {
+            Post::find($request->input('id'))->update($request->all());
+            return redirect()->back()->with('message', 'Post Updated Successfully.');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        if ($request->has('id')) {
+            Post::find($request->input('id'))->delete();
+            return redirect()->back();
+        }
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Inertia::share([
+
+            'errors' => function () {
+
+                return Session::get('errors')
+
+                    ? Session::get('errors')->getBag('default')->getMessages()
+
+                    : (object) [];
+
+            },
+
+        ]);
+
+  
+
+        Inertia::share('flash', function () {
+
+            return [
+
+                'message' => Session::get('message'),
+
+            ];
+
+        });
     }
 }
